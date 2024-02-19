3c12541497e3d9c6856c1fae3b5e176dfe6520c3
User <Willspri@gmail.com>
2024-02-19 15:07:30 +1300

    Add project files.

---
+++ PictureBox/Form1.cs
@@ -0,0 +1,26 @@
+namespace PictureBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.ImageLocation = ofd.FileName;
+                }
+            }
+        }
+    }
+}
